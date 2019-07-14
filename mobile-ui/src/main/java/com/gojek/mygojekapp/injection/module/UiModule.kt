5cfcd002df25f8a6743a5e977f3a249453558a96
Gyanendra Singh <gyanendra.sirohi@yatra.com>
2019-07-14 17:29:15 +0530

    providing UIThred for post usecase execution through dagger

--- mobile-ui/src/main/java/com/gojek/mygojekapp/injection/module/UiModule.kt
+++ mobile-ui/src/main/java/com/gojek/mygojekapp/injection/module/UiModule.kt
@@ -1,11 +1,17 @@
 package com.gojek.mygojekapp.injection.module
 
+import com.gojek.mygojekapp.UiThread
 import com.gojek.mygojekapp.forecast.ForecastActivity
+import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import org.buffer.android.boilerplate.domain.executor.PostExecutionThread
 
 @Module
 abstract class UiModule{
+
+    @Binds
+    abstract fun bindPostExecutionThread(uiThread: UiThread): PostExecutionThread
 
     @ContributesAndroidInjector
     abstract fun contributeForecastActivity():ForecastActivity
