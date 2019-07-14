54a6c68e31e131114ce1f2d8730363aa0b37176c
Gyanendra Singh <gyanendra.sirohi@yatra.com>
2019-07-14 16:51:01 +0530

    Domain Layer implementation with UseCases based on the business logic

---
+++ domain/src/main/java/com/gojek/domain/interactor/forecast/GetForecast.kt
@@ -0,0 +1,20 @@
+package com.gojek.domain.interactor.forecast
+
+import com.gojek.domain.datamanager.WeatherDataManager
+import com.gojek.domain.interactor.FlowableUseCase
+import com.gojek.domain.model.ForecastData
+import com.gojek.domain.model.ForecastRequest
+import io.reactivex.Flowable
+import org.buffer.android.boilerplate.domain.executor.PostExecutionThread
+import org.buffer.android.boilerplate.domain.executor.ThreadExecutor
+import javax.inject.Inject
+
+class GetForecast @Inject constructor(private val dataManager: WeatherDataManager,
+                                      threadExecutor: ThreadExecutor,
+                                      postThreadExecutor: PostExecutionThread) :
+FlowableUseCase <ForecastData,ForecastRequest>(threadExecutor,postThreadExecutor){
+    override fun buildUseCaseObservable(params: ForecastRequest?): Flowable<ForecastData> {
+        return dataManager.getForecast(params!!.location,params!!.days)
+    }
+
+}
---
+++ domain/src/main/java/com/gojek/domain/model/ForecastRequest.kt
@@ -0,0 +1,3 @@
+package com.gojek.domain.model
+
+data class ForecastRequest(val location:String,val days:Int)
